07fc5feb30d223814965155dddd5dd4bbf4aed93
jdardoin <jdardoin@icloud.com>
2024-02-05 17:41:01 -0600

    add images for github/linkden and update footer components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+ //import github from '../../images/github.png'
+// import linkedin from '../../images/linkedin.jpg'
 
 const Footer = () => {
   return (
-    <div>index</div>
+    <div className='footerwraper'>
+
+      <a href="https://github.com/jdardoin">Github Profile</a>
+
+    </div>
   )
 }
 
 export default Footer
+
+// Could not get github/linkedin images or links  to display correctly.
