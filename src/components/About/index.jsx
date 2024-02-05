e4d877f2606a754424cf69be312de9521d33a0e8
jdardoin <jdardoin@icloud.com>
2024-02-05 00:33:26 -0600

    add profile pic and change about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,11 +6,11 @@ const About = () => {
 
     <div className="aboutcontainer">
       <div className="imgwarper"> 
-     < img src="https://i.imgur.com/8X3Q1ZT.jpg" alt="profile picture" width="200" height="200" />
+     < img src="src/images/profile.jpeg" alt="profile picture" width="400" height="500" />
       </div>
       <div className='aboutcontent'>
         <h1>About</h1>
-        <p>Hi, I'm a full-stack developer. I have experience with React, Node, Express, and MongoDB. I'm currently learning Python and Django. I'm also working on a few projects that I will be adding to my portfolio soon. I'm currently looking for a full-time position as a full-stack developer. If you have any questions or would like to get in touch, please feel free to contact me. </p>
+        <p>Hi, I'm a full-stack developer. I have experience with React, Node, Express, and MongoDB.. I'm also working on a few projects that I will be adding to my portfolio soon. I'm currently looking for a full-time position as a full-stack developer. If you have any questions or would like to get in touch, please feel free to contact me. </p>
     </div>
     </div>
   )
